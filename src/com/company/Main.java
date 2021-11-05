22db77d06edaf0883d1598a08b35bd727e378268
Ianis Chisiu <IC210452@reigate.ac.uk>
2021-11-05 12:05:37 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter barcode");
+        // int  digits = input.nextInt();
+        int digits[] = new int[14];
+        for (int i = 0; i < 14; i++) {
+            if ((i % 2) > 0) {
+                if
+
+                }
+            }
+
+        }
+
+
+    }
+
+}
+
